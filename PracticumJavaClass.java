056f6e8055ea163b87eab84161095f7196b5456f
morozvitali <morozvitali99@gmail.com>
2022-10-08 12:57:09 +0300

    vitaliComit

--- PracticumJavaClass.java
+++ PracticumJavaClass.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class PracticumJavaClass {
+public class PracticumJavaClass {
+    public static void main(String[] args) {
+        System.out.println("VitaliComitFile");
+    }
 }
