2a00c78707f40533fa6ffec7df0aeb6f0ff7c2eb
AndrewAntonyuk <antonyuk.andrij@gmail.com>
2022-10-22 12:49:57 +0300

    Add reverse of string

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,4 +1,15 @@
 package java;
 
 public class Main {
+    public String stringReverse(String str) {
+        char[] srcArr = str.toCharArray();
+        char[] trgArr = str.toCharArray();
+        int max = str.length() - 1;
+
+        for (int i = max; i >= 0; i--) {
+            trgArr[max - i] = srcArr[i];
+        }
+
+        return new String(trgArr);
+    }
 }
