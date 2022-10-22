33492da488ec1bf002312d24d626d558c3888f50
vhumenny <v.humenny@gmail.com>
2022-10-22 12:53:46 +0300

    new commit. made method

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,4 +1,17 @@
 package java;
 
 public class Main {
+    public static void main(String[] args){
+        String p = "asd";
+        isPalindrome(p)
+    }
+    public static boolean isPalindrome(String p) {
+        char[] array = p.toCharArray();
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            reversed.append(p.charAt(i));
+        }
+        return p.intern().equals(reversed.toString().intern());
+    }
 }
