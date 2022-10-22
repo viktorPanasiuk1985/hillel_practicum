7a9c4d121ed7bc6af86735729ac3d5f9f7891c29
KorbutViacheslav <minowaman777@gmail.com>
2022-10-22 12:55:50 +0300

    Add algoritm method

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,4 +1,10 @@
 package java;
 
 public class Main {
+    public static void main(String[] args) {
+        algoritm("Viacheslav","Korbut")
+    }
+    public static String algoritm(String name, String sourName){
+        System.out.println(name+" "+sourName);
+    }
 }
