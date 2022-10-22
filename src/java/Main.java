0919e17e2471d73240f224bf7cb424d124369b45
Vladimir008 <vladimir.podkidyshev@gmail.com>
2022-10-22 12:48:38 +0300

    CreateStringMethod

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,4 +1,13 @@
 package java;
 
 public class Main {
+
+    public static String stringReverse(String string) {
+        char[] chars = string.toCharArray();
+        String res = "";
+        for (int i = chars.length - 1; i >= 0; i--) {
+            res = res + chars[i];
+        }
+        return res;
+    }
 }
