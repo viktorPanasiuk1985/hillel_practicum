2e28a11352caf0e93871bd5e5cee1496bee4cb8a
vhumenny <v.humenny@gmail.com>
2022-10-23 08:45:57 +0300

    new commit. made method

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,4 +1,8 @@
 package java;
 
 public class Main {
+    public static void checkIfPositive(int e) {
+        if (e >= 0) System.out.println("The number " + e + " is positive.");
+        else System.out.println("The number " + e + " is negative.");
+    }
 }
