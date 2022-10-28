15a265777917a7ce244c9ecb25c05678530c6049
Lebedieva Helen <lebedievahelen@gmail.com>
2022-10-28 13:48:31 +0300

    add method

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,4 +1,9 @@
 package java;
 
 public class Main {
+
+    public static boolean isPalindrome(String string) {
+        StringBuilder newString = new StringBuilder(string).reverse();
+        return string.equalsIgnoreCase(String.valueOf(newString));
+    }
 }
